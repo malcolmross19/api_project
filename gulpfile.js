f306c0f56cb00fbb5fd1fea27186faa4c5755cab
Malcolm Ross <MJRoss@fullsail.edu>
2017-09-29 22:52:07 -0400

    Added tasks to gulp file.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', function() {
+
+});
+
+gulp.task('scss', function() {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function() {
+   gulp.watch('');
+});
