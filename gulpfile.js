4b69d6ed9ca3d9ac3c8acadaec5ee020a818be82
Malcolm Ross <MJRoss@fullsail.edu>
2017-10-01 00:34:38 -0400

    Modified gulpfile to watch for changes in any .scss files

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ gulp.task('default', function() {
 });
 
 gulp.task('scss', function() {
-    return gulp.src('./scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
+    return gulp.src('./scss/*.scss')
+        .pipe(sass())
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('sass:watch', function() {
-   gulp.watch('');
+gulp.task('watch', function() {
+   gulp.watch('./scss/**/*.scss', ['scss']);
 });
