a5b5fadedd30a499c788abdd5cbde3f76f2b8218
Malcolm Ross <MJRoss@fullsail.edu>
2017-10-03 21:43:42 -0400

    Added styling and logic for content retrieved via AJAX and JavaScript.

--- scripts/content.js
+++ scripts/content.js
@@ -7,54 +7,65 @@ const privateKey = '7244aae150027e21e7ca73af153c823f6af30304';
 
 
 // Listen for form submission
-submit.addEventListener('click', function(e) {
+submit.addEventListener("click", function(e) {
     // Capture value from input field
     var query = searchField.value;
 
     if (query) {
         // Set additional values for query
-        var limit = "9";
+        var limit = "10";
 
         // Build query string with a template literal
-        var url = apiEndPoint +=
-            '/v1/public/characters?nameStartsWith=' + query + '&limit=' + limit + '&apikey=' + publicKey;
+        var url =
+            'http://gateway.marvel.com/v1/public/characters?nameStartsWith=' + query + '&limit=' + limit + '&apikey=' + publicKey;
 
         // Instantiate new instance of XHR object
         var request = new XMLHttpRequest();
 
         // Send the request
         request.overrideMimeType('application/json');
-        request.open('GET', url, true);
-        request.send();
+        request.open('GET', url, false);
 
-        var responseObject = JSON.parse(request.responseText); // Parse the data from JSON
-        console.log(responseObject.data.results);
+        request.onreadystatechange = function() {
+            var responseObject = JSON.parse(request.responseText); // Parse the data from JSON
 
-        // Do something with the data
-        var comicsContent = '';
+            // Do something with the data
+            var comicsContent = '';
 
-        for (var i = 0; i < responseObject.data.results.length; i++) {
-            if(responseObject.data.results[i].comics.available > 0){
-                comicsContent += '<div class=comic>';
-                comicsContent += '<img src=' + responseObject.data.results[i].thumbnail.path + ' ';
-                comicsContent += 'alt=comic />'; //+ responseObject.data.results[i].name + ' />';
-                comicsContent += '</div>';
+            for (var i = 0; i < responseObject.data.results.length; i++) {
+                if(responseObject.data.results[i].comics.available > 0){
+                    comicsContent += '<div class=comic>';
+                    comicsContent += '<img src=' + responseObject.data.results[i].thumbnail.path + '.';
+                    comicsContent += responseObject.data.results[i].thumbnail.extension + ' ';
+                    comicsContent += 'alt=' + responseObject.data.results[i].name + ' />';
+                    comicsContent += '<div class=info>';
+                    comicsContent += '<p><b>' + responseObject.data.results[i].name + '</b><br />';
+                    if(responseObject.data.results[i].description == "" || responseObject.data.results[i].description == " "){
+                        comicsContent += 'No Description Available</p></div>';
+                    } else {
+                        comicsContent += responseObject.data.results[i].description + '</p></div>';
+                    }
+                    comicsContent += '</div>';
+                }
             }
-        }
 
-        // Update the page with content
-        var comics = document.querySelector('#searchResults');
+            // Add the header back to the page
+            var searchHeader = document.querySelector('#searchHeader');
+            searchHeader.style.display = 'inline-block';
 
-        if (comicsContent) {
-            comics.innerHTML = comicsContent;
-        }
+            // Update the page with content
+            var comics = document.querySelector('#searchResults');
+
+            if (comicsContent) {
+                comics.innerHTML = comicsContent;
+            }
+        };
+        request.send();
 
     } else {
         // Prevent default action from occurring
         e.preventDefault();
     }
-
-
 }, false);
 
 
@@ -63,10 +74,4 @@ window.addEventListener('load', function() {
     document.querySelector('#searchTerm').value = "";
 }, false);
 
-
-function GetHash() {
-    var date = Date.now();
-    var timeStamp = date.getMilliseconds();
-    var md5Hash = md5(timeStamp + privateKey + publicKey);
-}
 
